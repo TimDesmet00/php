8e64e52c12539fc36fd60de9414d84d77b6f751a
tim <timdesmet00@gmail.com>
2023-10-19 15:38:14 +0200

    drill exo 11.2

--- unicorn.php
+++ unicorn.php
@@ -1,21 +1,24 @@
 
 <?php
 
-if (isset $_GET['race']){
+if (isset ($_GET['race'])){
     $race = $_GET['race'];
     $image = "";
 
-    $image = ($race == "human") ? "human.gif" : ($race == "cat") ? "cat.gif" : ($race == "unicorn") "unicorn.gif";
+    $image = ($race == "human") ? "human.gif" : (($race == "cat") ? "cat.gif" : (($race == "unicorn") ? "unicorn.gif" : ""));
+
+    echo "<img src='$image' alt='image' />";
 }
 
 ?>
 
 <form method="get" action="">
 <label for="race">Are you a Human:</label>
-    <input type="radio" name="race" id="race" value="human">
+    <input type="radio" name="race" id="human" value="human">
     <label for="race">a Cat:</label>
-    <input type="radio" name="race" id="race" value="cat">
+    <input type="radio" name="race" id="cat" value="cat">
     <label for="race">a Unicorn</label>
-    <input type="radio" name="race" id="race" value="unicorn">
+    <input type="radio" name="race" id="unicorn" value="unicorn">
     <label for="race">?</label>
+    <input type="submit" name="submit" value="Greet me now">
 </form>

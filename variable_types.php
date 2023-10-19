aae1704711ee6b3429e78c887c3efb414f98c95a
tim <timdesmet00@gmail.com>
2023-10-19 09:16:39 +0200

    exo conditions

--- variable_types.php
+++ variable_types.php
@@ -23,6 +23,30 @@
         7 => "Stefan" ); ?>
     <p>La première personne de ma famille, c'est <?php echo $myFamily[0]; ?>.</p>
     <?php $hungry = true; ?>
+
+    <?php
+        $temperature = 14;
+        if( $temperature >= 15 ) {
+        // code to execute if the condition results TRUE
+            $clothes = "T-shirt";
+            $should_i_wear_a_scarf = false;
+        } else {
+        // code to execute if the condition results FALSE
+            $clothes = "Pull-over";
+            $should_i_wear_a_scarf = true;
+        }
+        
+        if ($should_i_wear_a_scarf == true){
+            // this is a "fake" function for the sake of the example 
+            // that function is only executed in the condition is true
+            // grab_scarf_from_door_hanger();
+        }
+        // The following function will be executed everytime, 
+        // but its $clothes argument changes according to the result
+        // of our previous conditional structure
+        // cover_my_chest_with($clothes);
+        echo $clothes;
+    ?>
     
 </body>
 </html>

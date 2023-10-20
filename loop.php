340affe9a334b29062d3db065e0bcb52df176b68
tim <timdesmet00@gmail.com>
2023-10-20 16:56:22 +0200

    debut exo loop

---
+++ loop.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$pronouns = array ('I', 'You', 'He/She','We', 'You', 'They');
+
+foreach ($pronouns as $pronoun) {
+    if ($pronoun == 'He/She') {
+        echo $pronoun . ' codes<br>';
+    } else {
+        echo $pronoun . ' code<br>';
+    }
+}

78764829d767998380297faeb4a0172a85c69a0f
tim <timdesmet00@gmail.com>
2023-10-23 10:07:36 +0200

    fin exercice loop

--- loop.php
+++ loop.php
@@ -10,3 +10,71 @@ foreach ($pronouns as $pronoun) {
         echo $pronoun . ' code<br>';
     }
 }
+
+// $number = 1;
+
+// while ($number <= 120) {
+//     echo $number . '<br>';
+//     $number++;
+// }
+
+// for ($number = 1; $number <= 120; $number++) {
+//     echo $number . '<br>';
+// }
+
+// $members = array (
+//     "Alexandre Vandewiele",
+//     "Antoine Lansman",
+//     "Bastien Venturi",
+//     "Carole Gerard",
+//     "Dorian Vanden Eynde",
+//     "Elisabeth Leyder",
+//     "Elodie Ali",
+//     "Justin Michel",
+//     "Justine Frigo",
+//     "Justine Leleu",
+//     "Kimi Lefort",
+//     "Layla",
+//     "Lidwine Careme",
+//     "Lucas Beauloi",
+//     "Marie Tara",
+//     "Mathias Barbier",
+//     "Okly 2023",
+//     "Pierre Marien",
+//     "Robin Piot",
+//     "Rosalie Boulard",
+//     "Stephane Comblez",
+//     "Tim Desmet",
+//     "Toms",
+//     "Valentin Lefort",
+//     "Vens Alexandre",
+//     "Virginie Dourson",
+// );
+
+// foreach ($members as $member) {
+//     echo $member . '<br>';
+// }
+
+$pays = array(
+    "BE" => "Belgique",
+    "FR" => "France",
+    "DE" => "Allemagne",
+    "NL" => "Pays-Bas",
+    "ES" => "Espagne",
+    "IT" => "Italie",
+    "PT" => "Portugal",
+    "GB" => "Grande-Bretagne",
+    "IE" => "Irlande",
+    "LU" => "Luxembourg"
+);
+
+?>
+<form>
+    <select name="pays">
+        <?php
+        foreach ($pays as $key => $value) {
+            echo '<option value="' . $key . '">' . $value . '</option>';
+        }
+        ?>
+    </select>
+</form>
